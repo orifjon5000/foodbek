6df062a6876d4f346aa09edd7949b8479e898013
orifjon <orifjon.5000@gmail.com>
2021-12-17 01:45:00 +0900

    add button

--- src/component/Drawer/Index.jsx
+++ src/component/Drawer/Index.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import { Drawer, Close } from './style'
+import React from "react";
+import { Drawer, Close } from "./style";
 
-export const Index=({children, open, onClos})=> {
-
-
-
+export const Index = ({ children, open, onClos }) => {
   return (
-    <Drawer open={open} >
-    <Close onClick={onClos}> X</Close>
-    {children}
+    <Drawer open={open}>
+      <Close onClick={onClos}> X</Close>
+      {children}
     </Drawer>
-  )
-}
-
-export default Index
+  );
+};
